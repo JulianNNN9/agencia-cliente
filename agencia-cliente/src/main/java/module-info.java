efe3a00762b64c88fcd0053b9abb528e94d83883
JulianNNN9 <111833091+JulianNNN9@users.noreply.github.com>
2023-11-09 21:04:16 -0500

    .

--- agencia-cliente/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module co.edu.uniquindio.agenciacliente {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens co.edu.uniquindio.agenciacliente to javafx.fxml;
-    exports co.edu.uniquindio.agenciacliente;
-}
